5e0b342ba969708c2a480a4ae86fde7a4c2920f4
Guilmar24 <91397336+Guilmar24@users.noreply.github.com>
2025-03-28 23:35:34 -0600

    Vistas
    creacion de vistas

---
+++ src/main/java/java/proyectoprogramacion25/HelloController.java
@@ -0,0 +1,14 @@
+package java.proyectoprogramacion25;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
